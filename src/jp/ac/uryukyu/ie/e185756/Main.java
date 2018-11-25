c31d18d1cd0cf4544bed7677c889fa424ab76933
Kazuhide Matsuda <e185756@ie.u-ryukyu.ac.jp>
2018-11-26 00:35:14 +0900

    3rd commit package 追加

---
+++ src/jp/ac/uryukyu/ie/e185756/Main.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e185756;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+//        try {
+//            String str = null;
+//            System.out.println(str.length());
+//        }catch(NullPointerException e) {
+//            System.out.println("NullPointerExceptionが発生");
+//            System.out.println(e.getMessage());
+//        }
+    }
+}
